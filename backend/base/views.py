d771f52c81576f774fbb40d4479d19f6f0b26c36
hassan omar <hassan.omar@naivas.co.ke>
2021-07-12 22:28:59 +0300

    initial

---
+++ backend/base/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from .products import products
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+# Create your views here.
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes= [
+        '/api/products',
+        '/api/products/create',
+    ]
+    return Response(routes)
+
+
+@api_view(['GET'])
+def getProducts(request):
+    return Response(products)
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    for i in products:
+        if i['_id'] == pk:
+            product = i
+            break
+    return Response(product)
