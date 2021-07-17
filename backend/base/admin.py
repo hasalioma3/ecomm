7518b80be1567fe16fb122f3011bc298bfda7b63
hasalioma3 <83871782+hasalioma3@users.noreply.github.com>
2021-07-17 13:36:01 +0300

    pulling from django
    pulling from django

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display=('user', 'name','category','rating', 'numReviews', 'price', 'countInStock', 'createdAt', )
+    list_per_page = 10
+    list_filter =['category',]
+@admin.register(Review)
+class ProductAdmin(admin.ModelAdmin):
+    list_per_page = 10
+@admin.register(Order)
+class ProductAdmin(admin.ModelAdmin):
+    list_per_page = 10
+@admin.register(OrderItem)
+class ProductAdmin(admin.ModelAdmin):
+    list_per_page = 10
+@admin.register(ShippingAddress)
+class ProductAdmin(admin.ModelAdmin):
+    list_per_page = 10
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -2,11 +2,9 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
 import Rating from "../components/Rating";
-// import products from "../products";
 import axios from "axios";
 
 function ProductScreen({ match }) {
-  // const product = products.find((p) => p._id === match.params.id);
   const [product, setProducts] = useState([]);
   useEffect(() => {
     async function fetchProduct() {
