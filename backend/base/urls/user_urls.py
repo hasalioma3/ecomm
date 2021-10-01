62e0f5dbaf5c217e746359aa1a40e0a883729c28
hasalioma3 <83871782+hasalioma3@users.noreply.github.com>
2021-10-01 12:30:03 +0300

    okay

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('login/', views.MyTokenObtainPairView.as_view(),
          name='token_obtain_pair'),
     path('profile/', views.getUserProfile, name='user_profile'),
+    path('profile/update/', views.updateUserProfile, name='user_profile_update'),
     path('', views.getUsers, name='users'),
 ]
